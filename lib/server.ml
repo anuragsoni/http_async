70317c8db163d24462fe0128c57e3eecfbbd4bf1
Anurag Soni <anurag@sonianurag.com>
2020-02-07 22:41:36 -0500

    split server into its own module

---
+++ lib/server.ml
@@ -0,0 +1,25 @@
+open! Core
+open Async
+
+let listen
+    ?buffer_age_limit
+    ?max_connections
+    ?max_accepts_per_batch
+    ?backlog
+    ?socket
+    ~on_handler_error
+    ~request_handler
+    ~error_handler
+    where_to_listen
+  =
+  Tcp.(
+    Server.create
+      ?buffer_age_limit
+      ?max_connections
+      ?max_accepts_per_batch
+      ?backlog
+      ?socket
+      ~on_handler_error
+      where_to_listen)
+    (Server0.create_connection_handler ~request_handler ~error_handler)
+;;
